f742190a004ea4d1f8415642c140435b676cc16f
aksi1984 <pablo_xmen@op.pl>
2022-02-13 14:49:55 +0100

    added operators for scalar operations

--- include/bits/Ops.hpp
+++ include/bits/Ops.hpp
@@ -53,6 +53,42 @@ namespace axilib
     }
 
     template<typename T, typename STORAGE>
+    Matrix<T, STORAGE> operator+(const Matrix<T, STORAGE>& lhs, const T& rhs)
+    {
+        Matrix<T, STORAGE> mat = lhs;
+        mat+= rhs;
+
+        return mat;
+    }
+
+    template<typename T, typename STORAGE>
+    Matrix<T, STORAGE> operator-(const Matrix<T, STORAGE>& lhs, const T& rhs)
+    {
+        Matrix<T, STORAGE> mat = lhs;
+        mat-= rhs;
+
+        return mat;
+    }
+
+    template<typename T, typename STORAGE>
+    Matrix<T, STORAGE> operator*(const Matrix<T, STORAGE>& lhs, const T& rhs)
+    {
+        Matrix<T, STORAGE> mat = lhs;
+        mat*= rhs;
+
+        return mat;
+    }
+
+    template<typename T, typename STORAGE>
+    Matrix<T, STORAGE> operator/(const Matrix<T, STORAGE>& lhs, const T& rhs)
+    {
+        Matrix<T, STORAGE> mat = lhs;
+        mat/= rhs;
+
+        return mat;
+    }
+
+    template<typename T, typename STORAGE>
     bool operator==(const Matrix<T, STORAGE>& lhs, const Matrix<T, STORAGE>& rhs)
     {
         return op::Equal<Matrix<T, STORAGE>, Matrix<T, STORAGE>, op::Op_matrix>()(lhs, rhs);
